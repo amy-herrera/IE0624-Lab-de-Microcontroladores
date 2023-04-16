cefb1898b6e6e0ca6474f9cd11f4ad6956c4c43c
Betelo995 <rojitashedez@gmail.com>
2023-04-16 17:07:09 -0600

    Se añadieron las resistencias correctas definidas de forma teórica para la simulación del circuito

--- Segundo Laboratorio/src/lavadora.c
+++ Segundo Laboratorio/src/lavadora.c
@@ -1,3 +1,11 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+
+#define F_CPU 1000000UL
+
+//Definición de los pines del microcontrolador:
+#define BTN_START_PAUSE (1 << PINA2)
+#define BTN_LOAD_LOW (1 << PINB0)
+#define BTN_LOAD_MED (1 <<)
+#define BTN_LOAD_HIGH (1 << PINB2)
