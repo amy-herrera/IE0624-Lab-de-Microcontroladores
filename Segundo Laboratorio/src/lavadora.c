26460dfdfd3d1fd2f62e2c52226f7d8689752ac9
Betelo995 <rojitashedez@gmail.com>
2023-04-16 18:06:27 -0600

    Se incluyeron las asignaciones de los pines

--- Segundo Laboratorio/src/lavadora.c
+++ Segundo Laboratorio/src/lavadora.c
@@ -2,10 +2,35 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define F_CPU 1000000UL
 
-//Definición de los pines del microcontrolador:
-#define BTN_START_PAUSE (1 << PINA2)
-#define BTN_LOAD_LOW (1 << PINB0)
-#define BTN_LOAD_MED (1 <<)
-#define BTN_LOAD_HIGH (1 << PINB2)
+// Definir pines de botones
+#define START_PAUSE_PIN PINA2
+#define HIGH_LOAD_PIN PINB2
+#define MEDIUM_LOAD_PIN PINB1
+#define LOW_LOAD_PIN PINB0
+
+// Definir pines de LEDs
+#define CENTRIFUGADO_LED_PIN PINB4
+#define ENJUAGADO_LED_PIN PINB3
+#define LAVAR_LED_PIN PIND5
+#define SUMINISTRO_AGUA_LED_PIN PIND4
+
+#define LOW_LOAD_LED_PIN PINB7
+#define MEDIUM_LOAD_LED_PIN PINB6
+#define HIGH_LOAD_LED_PIN PINB5
+
+// Definir pines de BCD a 7 segmentos
+#define BCD_A_PIN PINA0
+#define BCD_B_PIN PINA1
+#define BCD_C_PIN PIND1
+#define BCD_D_PIN PIND0
+
+
+//Configuración de los pines del microcontrolador.
+DDRA &= ~(1 << START_PAUSE_PIN);
+DDRB &= ~((1 << LOW_LOAD_PIN) | (1 << MEDIUM_LOAD_PIN) | (1 << HIGH_LOAD_PIN));
+DDRB |= (1 << LOW_LOAD_LED_PIN) | (1 << MEDIUM_LOAD_LED_PIN) | (1 << HIGH_LOAD_LED_PIN);
+DDRB |= (1 << ENJUAGADO_LED_PIN) | (1 << CENTRIFUGADO_LED_PIN);
+DDRD |= (1 << LAVAR_LED_PIN) | (1 << SUMINISTRO_AGUA_LED_PIN);
+DDRA |= (1 << BCD_A_PIN) | (1 << BCD_B_PIN);
+DRRD |= (1 << BCD_C_PIN) | (1 << BCD_D_PIN);
