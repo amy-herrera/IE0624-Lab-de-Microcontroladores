c7d04bfa4e5cebf63981aee62209264f4e056ce7
Amy Herrera <amyyhm11@gmail.com>
2023-05-06 00:30:10 -0600

    Actualizando version final

--- Tercer Laboratorio/src/datasaver.py
+++ Tercer Laboratorio/src/datasaver.py
@@ -24,7 +24,6 @@ contador = 0
 valores = []
 file = open(fileName, mode='w', newline='')
 file.write(encabezado)
- 
 num_datos = 100  # Número de datos que se esperan recibir antes de cerrar el puerto
 contador_datos = 0  # Contador de datos recibidos
 
@@ -48,75 +47,3 @@ while True:
 ser.close()
 file.close()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Abrir el archivo CSV en modo escritura
-'''with open(fileName, mode='w', newline='') as file:
-    # Se define el separador de campos como punto y coma
-    writer = csv.writer(file, delimiter=';')
-    writer.writerow(encabezado)
-
-
-    while True:
-        # Leer una línea desde el puerto serial
-        linea = ser.readline().decode('utf-8').strip()
-        
-        # Si la línea es vacía, salimos del ciclo
-        if not linea:
-            break
-        
-        # Separar los valores de la línea
-        valores = linea.split(',')
-        print(valores)
-        
-        linea = ";".join([str(valor) for valor in valores])
-        file.write(linea)
-        time.sleep(0.1)
-
-        
-        #writer.writerow([float(v.strip()) for v in valores])
-
-        
-        # Escribir los valores en el archivo CSV
-        #writer.writerow([str(v.strip()) for v in valores])
-        #linea = ";".join(valores) + "\n"
-        #file.write(linea)'''
-
-file.close()
-''' while True:
-    for c in ser.read():
-        c=chr(c)
-        print(c, end='')
-        line += c
-        
-        if c == "\n":
-            with open(fileName, 'w', newline='') as file:
-                writer = csv.writer(file, delimiter=';')
-                writer.writerows(line + "\n")
-                time.sleep(2)
-                file.close()
-           #file = open(fileName, "a")
-           	    time.sleep(2)
-           #file.write("La data obtenida se presenta acontinuacion: " + "\n")
-           #file.write(line + "\n")
-           #file.write("\n")
-           	    file.close() '''
